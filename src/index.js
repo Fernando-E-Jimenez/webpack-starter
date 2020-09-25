f916655d5ca35dc1f0694fb28bf2516205f6b4de
Fernando <ferve91@gmail.com>
2020-09-25 14:36:18 -0300

    Mi Primer Commit de TODO

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
-const nombre = 'Fernando';
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-saludar( nombre );
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+// todoList.todos.forEach(crearTodoHtml);
+// como el "todo" es el unico argumento que voy a mandar
+// se puede hacer de la segunda forma
